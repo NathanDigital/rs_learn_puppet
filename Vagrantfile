f5ddb76b94569c6ce14e545a3f975b9a2f4e5847
Nathan Dunn <nathan@nathan.digital>
2016-01-07 17:58:48 +0000

    added ssl site with selfsigned certificate

--- Vagrantfile
+++ Vagrantfile
@@ -4,4 +4,6 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.provision "shell", path: "userdata.sh"
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 443, host: 4443
 end
--- puppet/modules/rs_learn_puppet/manifests/web.pp
+++ puppet/modules/rs_learn_puppet/manifests/web.pp
@@ -8,10 +8,34 @@ class rs_learn_puppet::web {
     force  => true,
     purge  => true,
     recurse => true,
-    source => 'puppet:///modules/rs_learn_puppet/files/www/default'
+    source => 'puppet:///modules/rs_learn_puppet/www/default'
   }
   apache::vhost { 'vhost.example.com':
     port    => '80',
     docroot => '/var/www/vhost',
   }
+  file { '/var/www/ssl':
+    ensure => directory,
+    group  => 'www-data',
+    force  => true,
+    purge  => true,
+    recurse => true,
+    source => 'puppet:///modules/rs_learn_puppet/www/ssl'
+  }
+  file { '/etc/apache2/ssl':
+    ensure => directory,
+    force  => true,
+    purge  => true,
+    recurse => true,
+    source => 'puppet:///modules/rs_learn_puppet/ssl'
+  }
+  apache::vhost { 'ssl.rs_learn_puppet.local':
+    subscribe => File['/etc/apache2/ssl'],
+    require    => File['/var/www/ssl'],
+    port     => '443',
+    docroot  => '/var/www/ssl',
+    ssl      => true,
+    ssl_cert => '/etc/apache2/ssl/rs_learn_puppet.local.crt',
+    ssl_key  => '/etc/apache2/ssl/rs_learn_puppet.local.key'
+  }  
 }
