26e28fcc4282c9b2086928c94670fb485ad3072c
Nathan Dunn <nathan@nathan.digital>
2016-01-07 15:00:56 +0000

    fixed syntax in puppet/modules/rs_learn_puppet/manifests/web.pp

--- puppet/modules/rs_learn_puppet/manifests/web.pp
+++ puppet/modules/rs_learn_puppet/manifests/web.pp
@@ -4,10 +4,10 @@ class rs_learn_puppet::web {
   }
   file { '/var/www/default':
     ensure => directory,
-    group  => 'www-data'
-    force  => true
-    purge  => true
-    recurse => true
+    group  => 'www-data',
+    force  => true,
+    purge  => true,
+    recurse => true,
     source => 'puppet:///modules/rs_learn_puppet/files/www/default'
   }
   apache::vhost { 'vhost.example.com':
